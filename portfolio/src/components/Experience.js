700378fb7eab65cd8857fa58d5e6a1abf8cab4c5
Maliha Fatima <malihameer@hotmail.com>
2024-05-21 00:37:03 -0700

    added contact and experience

---
+++ portfolio/src/components/Experience.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Experience.css'
+export const Experience = () => {
+  return (
+    <div className='main-contianer'>
+        <p> Experience</p>
+       
+    </div>
+  )
+}
+
+export default  Experience;
