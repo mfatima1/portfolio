8f28594d522098f5cb48c9d8c3bb76bc33d888ab
Maliha Fatima <malihameer@hotmail.com>
2024-05-20 21:08:01 -0700

    added icons

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
-import { fadeInLeft, fadeInRight } from 'react-animations';
+import { fadeInLeft, fadeInRight, bounce } from 'react-animations';
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { CircularProgressbar ,buildStyles} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import AnimatedProgressProvider from "./AnimatedProgressProvider";
-
 import { easeQuadInOut } from "d3-ease";
+import { FaHtml5 } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaFigma } from "react-icons/fa";
+import { DiCss3 } from "react-icons/di";
+import { DiVisualstudio } from "react-icons/di";
+import { FaReact } from "react-icons/fa";
+import { TbFileTypeSql } from "react-icons/tb";
+import { DiJsBadge } from "react-icons/di";
+import { DiBootstrap } from "react-icons/di";
+
 import "./About.css";
 
 const fadeInLeftAnimation = keyframes`${fadeInLeft}`;
 const fadeInRightAnimation = keyframes`${fadeInRight}`;
-
+const bounceAnimation = keyframes`${bounce}`;
 
 const Box1 = styled.div`
     animation: 1s ${fadeInLeftAnimation};
@@ -21,12 +30,45 @@ const Box1 = styled.div`
 const Box2 = styled.div`
     animation: 1s ${fadeInRightAnimation};
 `;
+const moveUp = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-15px);
+  }
+`;
+
+const moveDown = keyframes`
+  0% {
+    transform: translateY(-15px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const IconWrapper = styled.div`
+display: inline-block;
+transition: transform 0.3s;
+animation: ${moveDown} 0.3s forwards;
+
+&:hover {
+  animation: ${moveUp} 0.3s forwards;
+}
+
+& > svg {
+  border-radius: 50%;
+  padding: 10px;
+  background: linear-gradient(to right, #050525, #2f003d, #0e2954);
+}
+`;
 
 export const About = () => {
 
       return (
         <div className='section2'>
-            <h2>Education</h2>
+            <p className='header1'>Education</p>
             <div className='ed-contain'>
                 <Box1 className='box1'>
                 <strong><p>University of California, Riverside</p></strong>  
@@ -118,7 +160,18 @@ export const About = () => {
                     </div>
                 
                 </div>
-           
+        <p className='subheading'>Tools/Frameworks/Language</p>
+        <div className='icons'>
+        <IconWrapper className="html-icon"><FaHtml5 /></IconWrapper>
+        <IconWrapper className="css-icon"><DiCss3 /></IconWrapper>
+        <IconWrapper className="github-icon"><FaGithub /></IconWrapper>
+        <IconWrapper className="figma-icon"><FaFigma /></IconWrapper>
+        <IconWrapper className="vscode-icon"><DiVisualstudio /></IconWrapper>
+        <IconWrapper className="react-icon"><FaReact /></IconWrapper>
+        <IconWrapper className="sql-icon"><TbFileTypeSql /></IconWrapper>
+        <IconWrapper className="js-icon"><DiJsBadge /></IconWrapper>
+        <IconWrapper className="bootstrap-icon"><DiBootstrap /></IconWrapper>
+        </div>
         </div>
     );
 };
