6f756202fbc7008a2ee67f4095c6f0626cb6d2e9
Maliha Fatima <malihameer@hotmail.com>
2024-05-22 21:41:17 -0700

    added contact info

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -1,7 +1,32 @@
 import React from 'react'
+import { MdOutlineMailOutline } from "react-icons/md"
+import { FaLinkedin } from "react-icons/fa";
+import { RiFilePaper2Line } from "react-icons/ri";
+import resumePDF from './MalihaFatimaResume.pdf'; 
 
+
+import './Contact.css'
 export const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='main'>
+      <div className='content'>
+        <div className='left'>
+      <h3 className='contact'>Contact</h3>
+      </div>
+      <div className='right'>
+      <a href="mailto:malihameer@hotmail.com" target="_blank" rel="noopener noreferrer" >
+            <p className='mail'>Email <MdOutlineMailOutline /></p>
+          </a>
+          <a href="https://www.linkedin.com/in/maliha-fatima-10525125b" target="_blank" rel="noopener noreferrer" >
+            <p className='linkedin'>LinkedIn <FaLinkedin /></p>
+          </a>
+          <a href={resumePDF} target="_blank" rel="noopener noreferrer">
+            <p className='resume'>Resume <RiFilePaper2Line /></p>
+            </a>
+      </div>
+      </div>
+    </div>
   )
 }
+
+export default Contact;
